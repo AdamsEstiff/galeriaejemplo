897bd6f7b233085d5cdb55873ce7b777ac89f92d
Adams <2kamishi2@gmail.com>
2018-04-10 18:11:56 -0600

    Initial commit

---
+++ app/src/main/java/cr/ac/unadeca/galeriaejemplo/subclases/ImageViewHolder.java
@@ -0,0 +1,26 @@
+package cr.ac.unadeca.galeriaejemplo.subclases;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.google.android.flexbox.FlexboxLayoutManager;
+
+import cr.ac.unadeca.galeriaejemplo.R;
+
+public class ImageViewHolder extends RecyclerView.ViewHolder {
+    public ImageView image;
+    public ImageViewHolder(View itemView) {
+        super(itemView);
+        image =  itemView.findViewById(R.id.image);
+    }
+    public void bindTo() {
+        ViewGroup.LayoutParams lp = image.getLayoutParams();
+        if (lp instanceof FlexboxLayoutManager.LayoutParams) {
+            FlexboxLayoutManager.LayoutParams flexboxLp = (FlexboxLayoutManager.LayoutParams)lp;
+            flexboxLp.setFlexGrow(1.0f);
+        }
+    }
+}
---
+++ app/src/main/java/cr/ac/unadeca/galeriaejemplo/subclases/RunImage.java
@@ -0,0 +1,10 @@
+package cr.ac.unadeca.galeriaejemplo.subclases;
+
+/**
+ * Created by pc on 12/3/2018.
+ */
+public class RunImage {
+    public String url;
+    public String author;
+    public String name;
+}
